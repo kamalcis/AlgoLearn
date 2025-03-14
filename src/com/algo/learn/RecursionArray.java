5899f2f85b257ea16cecd265adf86638f5b1a31d
kamal <kamalcis@gmail.com>
2025-03-14 16:46:31 +0600

    Linear Search with multiple target added

--- src/com/algo/learn/RecursionArray.java
+++ src/com/algo/learn/RecursionArray.java
@@ -1,12 +1,17 @@
 package com.algo.learn;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RecursionArray {
     
 	public static void main(String[] args) {
-		int[] arr = {1,2,3,7,5};
-		//System.out.println(isSorted(arr, 0));
+		int[] arr = {1,2,3,7,7,5};
+
 		
-		System.out.print(linearSearch(arr,15, 0));
+		List<Integer> lists = linearSearchMultipleTarget(arr, 7, 0, new ArrayList<>() );
+		System.out.println(lists);
 	}
 	
 	public static boolean isSorted(int[] arr, int index) {
@@ -36,4 +41,44 @@ public class RecursionArray {
 		  
 		  return linearSearch(arr, target, index+1);
 	}
+	
+	
+	public static List<Integer> linearSearchMultipleTarget(int[] arr, int target, int index, List<Integer> results) {
+		  
+		  
+		  
+		  if(index > arr.length-1  ) {
+			  return results;
+		  }
+		
+		  if(arr[index] == target) {
+			  results.add(index);
+		  }		  
+		
+		  
+		  return linearSearchMultipleTarget(arr, target, index+1, results);
+		  		  
+	}
+	
+	/*
+	public static List<Integer> linearSearchMultipleTargetAndResultInsideBody(int[] arr, int target, int index) {
+		  	  
+		  List<Integer> result = new ArrayList<Integer>();
+		  if(index > arr.length-1  ) {
+			  return null;
+		  }
+		
+		  if(arr[index] == target) {
+			  result.add(index);
+			  return result;
+		  }		  
+		
+		  
+		  List<Integer> individualResult =  linearSearchMultipleTargetAndResultInsideBody(arr, target, index+1);		  
+		  
+		  
+	}*/
+
+	
+	
 }
