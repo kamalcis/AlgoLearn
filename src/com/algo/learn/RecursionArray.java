aac302bcc7628b4aa6049282760b6c1f0028dcb2
kamal <kamalcis@gmail.com>
2025-03-14 15:39:55 +0600

    linear search added

--- src/com/algo/learn/RecursionArray.java
+++ src/com/algo/learn/RecursionArray.java
@@ -4,7 +4,9 @@ public class RecursionArray {
     
 	public static void main(String[] args) {
 		int[] arr = {1,2,3,7,5};
-		System.out.println(isSorted(arr, 0));
+		//System.out.println(isSorted(arr, 0));
+		
+		System.out.print(linearSearch(arr,15, 0));
 	}
 	
 	public static boolean isSorted(int[] arr, int index) {
@@ -21,5 +23,17 @@ public class RecursionArray {
 	}
 	
 	
-	
+	public static int linearSearch(int[] arr, int target, int index) {
+		
+		  if(index > arr.length-1  ) {
+			  return -1;
+		  }
+		
+		  if(arr[index] == target) {
+			  return index;
+		  }		  
+		
+		  
+		  return linearSearch(arr, target, index+1);
+	}
 }
